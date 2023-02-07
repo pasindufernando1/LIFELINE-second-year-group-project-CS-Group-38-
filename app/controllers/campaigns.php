d22eec9ab28472e2444a3bd5aa625fbfba8ef3d9
SW Dissanayake <snehadissanayaka56@gmail.com>
2023-02-07 11:22:58 +0530

    Donor UIs Completed

---
+++ app/controllers/campaigns.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+class Campaigns extends Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->view->render('campaigns');
+    }
+}
