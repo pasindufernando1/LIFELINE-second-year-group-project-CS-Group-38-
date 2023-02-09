e912760ddb35a3ea05f42075a62e5f2b88716fbd
Pasindu Fernando <2020cs051@stu.ucsc.cmb.ac.lk>
2023-02-09 15:53:50 +0530

    del
    del

--- app/config/databaseConf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'lifeline_final');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
--- app/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require 'config/DatabaseConf.php';
-    require 'libs/App.php';
-    require 'libs/Controller.php';
-    require 'libs/Database.php';
-    require 'libs/Model.php';
-    require 'libs/View.php';
-
-    $app = new App();
