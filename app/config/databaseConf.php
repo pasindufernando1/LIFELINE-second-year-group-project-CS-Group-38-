ac19229c406d59044bab9c76d34ce25d317d45e4
SnehaDissanayake <2020cs045@stu.ucsc.cmb.ac.lk>
2023-05-13 07:57:57 +0530

    donor minor issues fixed

--- app/config/databaseConf.php
+++ app/config/databaseConf.php
@@ -2,6 +2,6 @@
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'lifeline_final');
+define('DB_NAME', 'life_5_12');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
